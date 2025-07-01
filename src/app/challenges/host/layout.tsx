ffae6f270c3611902bc946268777d451440434ef
Pranesh Nikhar <praneshnikhar@gmail.com>
2025-07-01 16:38:04 +0000

    the navbar appears twice when i press host challenge

--- src/app/challenges/host/layout.tsx
+++ src/app/challenges/host/layout.tsx
@@ -1,14 +1,7 @@
-import { Header } from "@/components/header";
-
 export default function HostChallengeLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className="flex min-h-screen flex-col">
-      <Header />
-      <main className="flex-1">{children}</main>
-    </div>
-  );
+  return <>{children}</>;
 }
